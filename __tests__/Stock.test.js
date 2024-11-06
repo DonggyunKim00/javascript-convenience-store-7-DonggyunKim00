5270690386d3a75a560d24982ad4203def9d5ee5
donggyun <donggyunkim12@gmail.com>
2024-11-06 15:49:54 +0900

    feat(Stock): 결제된 상품의 수량만큼 해당 상품의 재고에서 차감 기능 구현
    - decreaseProducts 테스트 추가
    - find 메서드를 이용하여 해당하는 상품을 찾고, decrease 메서드를 호출

--- __tests__/Stock.test.js
+++ __tests__/Stock.test.js
@@ -21,4 +21,19 @@ describe('재고 테스트', () => {
       { name: '칫솔', price: 3000, quantity: 2, promotion: null },
     ]);
   });
+
+  test('결제된 상품의 수량만큼 해당 상품의 재고에서 차감할 수 있다.', () => {
+    const confirmProductsInfo = [
+      ['베지밀', 4],
+      ['면도기', 1],
+    ];
+
+    stock.decreaseProducts(confirmProductsInfo);
+
+    expect(stock.getProductsInfo()).toEqual([
+      { name: '베지밀', price: 1000, quantity: 5, promotion: '1+1' },
+      { name: '면도기', price: 5000, quantity: 4, promotion: null },
+      { name: '칫솔', price: 3000, quantity: 2, promotion: null },
+    ]);
+  });
 });
--- src/model/Stock.js
+++ src/model/Stock.js
@@ -8,6 +8,17 @@ class Stock {
   getProductsInfo() {
     return this.#products.map((product) => product.getInfo());
   }
+
+  decreaseProducts(confirmProductsInfo) {
+    confirmProductsInfo.forEach(([name, count]) => {
+      const product = this.#findConfirmProduct(name);
+      if (product) product.decrease(count);
+    });
+  }
+
+  #findConfirmProduct(name) {
+    return this.#products.find((product) => product.getInfo().name === name);
+  }
 }
 
 export default Stock;
