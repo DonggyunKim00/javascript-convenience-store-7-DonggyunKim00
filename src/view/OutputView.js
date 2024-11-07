eb59a8d8395498e4b0c77420eafa2868cce651e7
donggyun <donggyunkim12@gmail.com>
2024-11-07 17:19:35 +0900

    feat(OutputView): 출력 클래스 일부 구현
    - 환영 메시지, 상품 출력 관련 메시지 상수 정의
    - printIntro, printOneProduct 구현

---
+++ src/view/OutputView.js
@@ -0,0 +1,25 @@
+import { Console } from '@woowacourse/mission-utils';
+
+const OutputView = {
+  OUTPUT_MESSAGE: Object.freeze({
+    WELCOME_INTRO: '안녕하세요. W편의점입니다.\n현재 보유하고 있는 상품입니다.\n',
+    PRODUCT_INFO: (name, price, quantity, promotionName) =>
+      `- ${name} ${price}원 ${quantity} ${promotionName}`,
+  }),
+
+  printIntro() {
+    Console.print(this.OUTPUT_MESSAGE.WELCOME_INTRO);
+  },
+
+  printOneProduct(name, price, quantity, promotion) {
+    let stringQuantity = '재고 없음';
+    if (quantity) stringQuantity = `${quantity}개`;
+
+    let promotionName = '';
+    if (promotion) promotionName = promotion.name;
+
+    Console.print(this.OUTPUT_MESSAGE.PRODUCT_INFO(name, price, stringQuantity, promotionName));
+  },
+};
+
+export default OutputView;
