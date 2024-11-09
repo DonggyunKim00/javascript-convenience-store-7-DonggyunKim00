e66418b88ef58a7ee28312f0821b424e7fe7bd41
donggyun <donggyunkim12@gmail.com>
2024-11-09 23:24:05 +0900

    fix(validate): 에러 메시지 상수값 오타 수정

--- src/utils/validate.js
+++ src/utils/validate.js
@@ -3,7 +3,7 @@ import { parseOrderInput } from './parse.js';
 export const ERROR_MESSAGE = Object.freeze({
   WRONG_ORDER_FORM: '올바르지 않은 형식으로 입력했습니다. 다시 입력해 주세요.',
   DOES_NOT_EXIST_PRODUCT: '존재하지 않는 상품입니다. 다시 입력해 주세요.',
-  EXCEED_QUANTITY: '재고 수량을 초과하여 구매할 수 없습니다. 다시  입력해 주세요.',
+  EXCEED_QUANTITY: '재고 수량을 초과하여 구매할 수 없습니다. 다시 입력해 주세요.',
   WRONG_INPUT: '잘못된 입력입니다. 다시 입력해 주세요.',
 });
 
