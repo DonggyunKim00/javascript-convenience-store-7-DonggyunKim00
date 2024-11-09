5b39b1fe6c12cd5e7f9dec8f166c961ea7e797ef
donggyun <donggyunkim12@gmail.com>
2024-11-09 18:25:42 +0900

    feat(PromotionService): 프로모션 입력 처리에 관한 기능 구현
    - 프로모션 입력 처리에 따른 결과 테스트 추가
    - 입력된 Order에 대하여 적절한 입력 요청을 보낼 수 있는 기능 구현

---
+++ __tests__/PromotionService.test.js
@@ -0,0 +1,222 @@
+import { MissionUtils } from '@woowacourse/mission-utils';
+import PromotionService from '../src/service/PromotionService';
+
+const mockQuestions = (inputs) => {
+  const messages = [];
+
+  MissionUtils.Console.readLineAsync = jest.fn((prompt) => {
+    messages.push(prompt);
+    const input = inputs.shift();
+
+    if (input === undefined) {
+      throw new Error('NO INPUT');
+    }
+
+    return Promise.resolve(input);
+  });
+
+  MissionUtils.Console.readLineAsync.messages = messages;
+};
+
+describe('프로모션 서비스 테스트', () => {
+  let result;
+
+  beforeEach(() => {
+    result = [];
+  });
+
+  test.each([
+    {
+      info: { name: '프로틴쉐이크', price: 3500, quantity: 4, promotion: { buy: 1, get: 1 } },
+      quantity: 3,
+      amount: 3,
+      isPromotionValid: false,
+      inputs: ['Y'],
+      answer: {
+        product: { name: '프로틴쉐이크', price: 3500, quantity: 4, promotion: { buy: 1, get: 1 } },
+        orderAmount: 3,
+        presentAmount: 0,
+      },
+    },
+    {
+      info: { name: '프로틴쉐이크', price: 3500, quantity: 4, promotion: { buy: 1, get: 1 } },
+      quantity: 3,
+      amount: 3,
+      isPromotionValid: false,
+      inputs: ['N'],
+      answer: {
+        product: { name: '프로틴쉐이크', price: 3500, quantity: 4, promotion: { buy: 1, get: 1 } },
+        orderAmount: 0,
+        presentAmount: 0,
+      },
+    },
+  ])(
+    '유효하지 않은 프로모션을 그대로 결제할지 결정할 수 있다.',
+    async ({ info, quantity, amount, isPromotionValid, inputs, answer }) => {
+      mockQuestions(inputs);
+
+      await PromotionService.promotionInputSystem(
+        { info, quantity, amount, isPromotionValid },
+        result,
+      );
+
+      expect(result).toEqual([answer]);
+    },
+  );
+
+  test.each([
+    {
+      info: { name: '오렌지주스', price: 1800, quantity: 9, promotion: { buy: 1, get: 1 } },
+      quantity: 7,
+      amount: 7,
+      isPromotionValid: true,
+      inputs: ['Y'],
+      answer: {
+        product: { name: '오렌지주스', price: 1800, quantity: 9, promotion: { buy: 1, get: 1 } },
+        orderAmount: 2,
+        presentAmount: 1,
+      },
+    },
+    {
+      info: { name: '오렌지주스', price: 1800, quantity: 9, promotion: { buy: 1, get: 1 } },
+      quantity: 3,
+      amount: 3,
+      isPromotionValid: true,
+      inputs: ['N'],
+      answer: {
+        product: { name: '오렌지주스', price: 1800, quantity: 9, promotion: { buy: 1, get: 1 } },
+        orderAmount: 1,
+        presentAmount: 0,
+      },
+    },
+  ])(
+    '1+1 상품에서 무료로 상품을 더 받을지 결정할 수 있다.',
+    async ({ info, quantity, amount, isPromotionValid, inputs, answer }) => {
+      mockQuestions(inputs);
+
+      await PromotionService.promotionInputSystem(
+        { info, quantity, amount, isPromotionValid },
+        result,
+      );
+
+      expect(result).toEqual([answer]);
+    },
+  );
+
+  test.each([
+    {
+      info: { name: '감자칩', price: 1500, quantity: 5, promotion: { buy: 1, get: 1 } },
+      quantity: 5,
+      amount: 8,
+      isPromotionValid: true,
+      inputs: ['Y'],
+      answer: {
+        product: { name: '감자칩', price: 1500, quantity: 5, promotion: { buy: 1, get: 1 } },
+        orderAmount: 8,
+        presentAmount: 2,
+      },
+    },
+    {
+      info: { name: '감자칩', price: 1500, quantity: 5, promotion: { buy: 1, get: 1 } },
+      quantity: 5,
+      amount: 10,
+      isPromotionValid: true,
+      inputs: ['N'],
+      answer: {
+        product: { name: '감자칩', price: 1500, quantity: 5, promotion: { buy: 1, get: 1 } },
+        orderAmount: 4,
+        presentAmount: 2,
+      },
+    },
+  ])(
+    '1+1 상품에서 프로모션을 못받는 상품을 그대로 결제할지 결정할 수 있다.',
+    async ({ info, quantity, amount, isPromotionValid, inputs, answer }) => {
+      mockQuestions(inputs);
+
+      await PromotionService.promotionInputSystem(
+        { info, quantity, amount, isPromotionValid },
+        result,
+      );
+
+      expect(result).toEqual([answer]);
+    },
+  );
+
+  test.each([
+    {
+      info: { name: '탄산수', price: 1200, quantity: 5, promotion: { buy: 2, get: 1 } },
+      quantity: 2,
+      amount: 2,
+      isPromotionValid: true,
+      inputs: ['Y'],
+      answer: {
+        product: { name: '탄산수', price: 1200, quantity: 5, promotion: { buy: 2, get: 1 } },
+        orderAmount: 3,
+        presentAmount: 1,
+      },
+    },
+    {
+      info: { name: '콜라', price: 1000, quantity: 10, promotion: { buy: 2, get: 1 } },
+      quantity: 8,
+      amount: 8,
+      isPromotionValid: true,
+      inputs: ['N'],
+      answer: {
+        product: { name: '콜라', price: 1000, quantity: 10, promotion: { buy: 2, get: 1 } },
+        orderAmount: 8,
+        presentAmount: 2,
+      },
+    },
+  ])(
+    '2+1 상품에서 무료로 상품을 더 받을지 결정할 수 있다.',
+    async ({ info, quantity, amount, isPromotionValid, inputs, answer }) => {
+      mockQuestions(inputs);
+
+      await PromotionService.promotionInputSystem(
+        { info, quantity, amount, isPromotionValid },
+        result,
+      );
+
+      expect(result).toEqual([answer]);
+    },
+  );
+
+  test.each([
+    {
+      info: { name: '사이다', price: 1000, quantity: 8, promotion: { buy: 2, get: 1 } },
+      quantity: 8,
+      amount: 10,
+      isPromotionValid: true,
+      inputs: ['Y'],
+      answer: {
+        product: { name: '사이다', price: 1000, quantity: 8, promotion: { buy: 2, get: 1 } },
+        orderAmount: 10,
+        presentAmount: 2,
+      },
+    },
+    {
+      info: { name: '탄산수', price: 1200, quantity: 5, promotion: { buy: 2, get: 1 } },
+      quantity: 5,
+      amount: 5,
+      isPromotionValid: true,
+      inputs: ['N'],
+      answer: {
+        product: { name: '탄산수', price: 1200, quantity: 5, promotion: { buy: 2, get: 1 } },
+        orderAmount: 4,
+        presentAmount: 1,
+      },
+    },
+  ])(
+    '2+1 상품에서 프로모션을 못받는 상품을 그대로 결제할지 결정할 수 있다.',
+    async ({ info, quantity, amount, isPromotionValid, inputs, answer }) => {
+      mockQuestions(inputs);
+
+      await PromotionService.promotionInputSystem(
+        { info, quantity, amount, isPromotionValid },
+        result,
+      );
+
+      expect(result).toEqual([answer]);
+    },
+  );
+});
---
+++ src/service/PromotionService.js
@@ -0,0 +1,63 @@
+import { readAndValidateFreeAddition, readAndValidatePurchase } from '../utils/readInput.js';
+
+class PromotionService {
+  static async promotionInputSystem({ info, quantity, amount, isPromotionValid }, result) {
+    if (!isPromotionValid)
+      return await PromotionService.#whenInvalidPromotion(info, amount, result);
+    if (info.promotion.buy === 1 && isPromotionValid)
+      return await PromotionService.#whenBuyOnePlusOne(info, quantity, amount, result);
+    if (info.promotion.buy === 2 && isPromotionValid)
+      return await PromotionService.#whenBuyTwoPlusOne(info, quantity, amount, result);
+    return null;
+  }
+
+  static async #whenInvalidPromotion(product, amount, result) {
+    const answer = await readAndValidatePurchase(product.name, amount);
+    let orderAmount = amount;
+    if (answer === 'N') orderAmount = 0;
+    await result.push({ product, orderAmount, presentAmount: 0 });
+  }
+
+  static async #whenBuyOnePlusOne(product, quantity, amount, result) {
+    const nonePromotion = amount - quantity + product.promotion.get;
+
+    if (amount < product.quantity && amount % 2 === 1)
+      return await PromotionService.#pushAdditionResult(product, 1, result);
+    if (amount >= product.quantity)
+      return await PromotionService.#pushPurchaseResult(product, amount, nonePromotion, result);
+    return result.push({ product, orderAmount: amount, presentAmount: quantity / 2 });
+  }
+
+  static async #whenBuyTwoPlusOne(product, quantity, amount, result) {
+    const nonePromotion = amount - quantity + product.promotion.get;
+    if (amount >= product.quantity)
+      return await PromotionService.#pushPurchaseResult(product, amount, nonePromotion, result);
+    if (quantity === amount && amount % 3 === 1)
+      return await PromotionService.#pushPurchaseResult(product, amount, nonePromotion, result);
+    if (quantity === amount && amount % 3 === 2)
+      return await PromotionService.#pushAdditionResult(product, quantity, result);
+    return result.push({ product, orderAmount: amount, presentAmount: quantity / 3 });
+  }
+
+  static async #pushPurchaseResult(product, amount, nonePromotion, result) {
+    const answer = await readAndValidatePurchase(product.name, nonePromotion);
+    let orderAmount = amount;
+    if (answer === 'N') orderAmount -= nonePromotion;
+    const maxQuantityPerPromotion = product.promotion.get + product.promotion.buy;
+    const maxPresentAmount = Math.floor(product.quantity / maxQuantityPerPromotion);
+    const presentAmount = Math.floor(
+      Math.min(maxPresentAmount, orderAmount / maxQuantityPerPromotion),
+    );
+    result.push({ product, orderAmount, presentAmount });
+  }
+
+  static async #pushAdditionResult(product, quantity, result) {
+    const answer = await readAndValidateFreeAddition(product.name);
+    let orderAmount = quantity;
+    if (answer === 'Y') orderAmount += 1;
+    const presentAmount = Math.floor(orderAmount / (product.promotion.get + product.promotion.buy));
+    result.push({ product, orderAmount, presentAmount });
+  }
+}
+
+export default PromotionService;
