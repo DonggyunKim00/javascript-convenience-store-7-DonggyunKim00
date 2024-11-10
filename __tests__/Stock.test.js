b9b3d55b1de3c37d749c9ec3dbeeff1b0177d544
donggyun <donggyunkim12@gmail.com>
2024-11-10 15:26:08 +0900

    refactor(Stock): 사용하지 않는 코드 제거

--- __tests__/Stock.test.js
+++ __tests__/Stock.test.js
@@ -21,30 +21,6 @@ describe('재고 테스트', () => {
     ]);
   });
 
-  test('결제된 상품의 수량만큼 해당 상품의 재고에서 차감할 수 있다.', () => {
-    const confirmProductsInfo = [
-      ['베지밀', 4],
-      ['면도기', 1],
-    ];
-
-    stock.decreaseProducts(confirmProductsInfo);
-
-    expect(stock.getProductsInfo()).toEqual([
-      { name: '베지밀', price: 1000, quantity: 5, promotion: '1+1' },
-      { name: '면도기', price: 5000, quantity: 4, promotion: null },
-      { name: '칫솔', price: 3000, quantity: 2, promotion: null },
-    ]);
-  });
-
-  test('등록되어 있지 않은 상품이라면 예외처리한다.', () => {
-    const confirmProductsInfo = [
-      ['제주산흑돼지', 4],
-      ['면도기', 1],
-    ];
-
-    expect(() => stock.decreaseProducts(confirmProductsInfo)).toThrow('[ERROR]');
-  });
-
   test.each(['베지밀', '면도기', '칫솔'])('%s 은(는) 재고에 존재한다.', (name) => {
     expect(stock.hasProductNameInStock(name)).toBe(true);
   });
--- src/model/Stock.js
+++ src/model/Stock.js
@@ -9,20 +9,6 @@ class Stock {
     return this.#products.map((product) => product.getInfo());
   }
 
-  decreaseProducts(confirmProductsInfo) {
-    confirmProductsInfo.forEach(([name, count]) => {
-      const product = this.#findConfirmProduct(name);
-      if (product) product.decrease(count);
-    });
-  }
-
-  #findConfirmProduct(name) {
-    const product = this.#products.find((item) => item.getInfo().name === name);
-
-    if (!product) throw new Error('[ERROR]');
-    return product;
-  }
-
   canDecreaseQuantityInStock(name, quantity) {
     const totalQuantity = this.getProductsInStockByName(name)
       .map((product) => product.getInfo().quantity)
